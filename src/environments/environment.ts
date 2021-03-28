d3938ecd235adab14fd75ca29f6fb80c11d19700
Fabiola Alvarado <alexfabiola28@gmail.com>
2021-03-28 17:40:59 -0400

    MDS-1: Firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPGnIH8jB6kn1uGtOjh0ZPWhEMC60Dtvc",
+    authDomain: "md-solutions-test.firebaseapp.com",
+    databaseURL: "https://md-solutions-test-default-rtdb.firebaseio.com",
+    projectId: "md-solutions-test",
+    storageBucket: "md-solutions-test.appspot.com",
+    messagingSenderId: "451078848156",
+    appId: "1:451078848156:web:cc3a5a7577851ee571f759"
+  }
 };
 
 /*
