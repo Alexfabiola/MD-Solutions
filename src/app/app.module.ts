87b304c2dc80efbd6ecfb127287634bd95e87e15
Fabiola Alvarado <alexfabiola28@gmail.com>
2021-03-28 19:00:25 -0400

    MDS-2: sign in component/service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
   ],
   imports: [
     BrowserModule,
